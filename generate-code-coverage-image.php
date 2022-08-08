05ed3bdfc2fbe6cb28a8e53af69fc161b37b6a99
benjaminknecht <knecht.benjamin@yahoo.de>
2022-08-08 14:56:37 +0200

    test

--- generate-code-coverage-image.php
+++ generate-code-coverage-image.php
@@ -3,8 +3,7 @@
 if (file_exists('coverage.xml')) {
     $xml = simplexml_load_file('coverage.xml');
 
-    echo '<pre>';
-    print_r($xml);
+    echo $xml->asXML();
 } else {
     exit('Konnte coverage.xml nicht öffnen.');
 }
