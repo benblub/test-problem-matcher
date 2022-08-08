a5994c6f412edc38878db6580bfe677c5838e4df
benjaminknecht <knecht.benjamin@yahoo.de>
2022-08-08 15:29:34 +0200

    test

--- generate-code-coverage-image.php
+++ generate-code-coverage-image.php
@@ -1,10 +1,1 @@
-<?= sprintf('::set-output name=var::%s', json_encode([
-    [
-        'id' => 'bar',
-        'name' => 'foo',
-    ],
-    [
-        'id' => 'bar2',
-        'name' => 'foo2',
-    ],
-]));
+<?= sprintf('::set-output name=var::%s', json_encode(simplexml_load_file('coverage.xml')));
